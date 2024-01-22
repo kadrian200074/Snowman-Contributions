c93582c82e35c012ce669328826606ae373c264a
mbmoreinis <143663172+mbmoreinis@users.noreply.github.com>
2024-01-22 10:32:10 -0500

    Update script-kc.js

--- script-kc.js
+++ script-kc.js
@@ -1,12 +1,23 @@
-/* PATCH NAME: Snowman Background
-* PATCH EFFECT: Adds a background image to the snowman
+/* PATCH NAME: Check Background
+* Change background in script to snowfall. 
+* Shift background to yellow at errors = 4; 
+* shift to red at errors = 5.
 */
-
-function setup(){
-  @ anywhere
-  + let snowman = document.getElementById("snowman");
-  + snowman.style.backgroundImage = "url('images/snow.jpg')";
-  + snowman.style.backgroundRepeat = "no-repeat";
-  + snowman.style.backgroundSize = "cover";
-  + snowman.style.backgroundColor = "#c6d1d3";
+*// Add into function badGuess();
+function checkBackground() {
+    +let snowman = document.getElementById ("snowman");
+    +if(nextPartId == 4){
+    +snowman.classList.add("yellowshift");
+    +}
+    +else if (nextPartId == 5) {
+    +snowman.classList.add("redshift");
+  }
 }
+
+// Add classes to css 
++ .yellowshift{
++ filter: hue-rotate(170deg);
++ }
++ .redshift{
++  filter: hue-rotate(135deg);
++ }
